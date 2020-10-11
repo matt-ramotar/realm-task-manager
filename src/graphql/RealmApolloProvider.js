532ffa4a124ea2036f8202527527f841ca513325
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-10 18:56:09 -0700

    Connect Apollo to GraphQL API

--- src/graphql/RealmApolloProvider.js
+++ src/graphql/RealmApolloProvider.js
@@ -1,17 +1,11 @@
-import React from "react";
-import { useRealmApp } from "../RealmApp";
-import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
-
+import React from 'react';
+import { useRealmApp } from '../RealmApp';
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 // Create an ApolloClient that connects to the provided Realm.App's GraphQL API
-const createRealmApolloClient = (app) => {
+const createRealmApolloClient = app => {
   const link = new HttpLink({
-     // TODO: Add your Realm App ID to the uri link to connect your app. 
+    // TODO: Add your Realm App ID to the uri link to connect your app.
     uri: `https://realm.mongodb.com/api/client/v2.0/app/${app.id}/graphql`,
     // A custom fetch handler adds the logged in user's access token to GraphQL requests
     fetch: async (uri, options) => {
@@ -20,8 +14,9 @@ const createRealmApolloClient = (app) => {
       }
       // Refreshing a user's custom data also refreshes their access token
       await app.currentUser.refreshCustomData();
-       // TODO: Include the current user's access token in an Authorization header with 
-       // every request. 
+      // TODO: Include the current user's access token in an Authorization header with
+      // every request.
+      options.headers.Authorization = `Bearer ${app.currentUser.accessToken}`;
       return fetch(uri, options);
     },
   });
@@ -32,6 +27,11 @@ const createRealmApolloClient = (app) => {
 };
 
 export default function RealmApolloProvider({ children }) {
-   // TODO: Create an ``ApolloClient`` object that connects to your app.
+  // TODO: Create an ``ApolloClient`` object that connects to your app.
+  const app = useRealmApp();
+  const [client, setClient] = React.useState(createRealmApolloClient(app));
+  React.useEffect(() => {
+    setClient(createRealmApolloClient(app));
+  }, [app]);
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
