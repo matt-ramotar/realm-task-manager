88efdfd8ca0256de636907eef72fbfd8b1266f2c
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-10 19:02:57 -0700

    Implement project list

--- src/graphql/useProjects.js
+++ src/graphql/useProjects.js
@@ -1,11 +1,11 @@
-import { useRealmApp } from "../RealmApp";
+import { useRealmApp } from '../RealmApp';
 
 export default function useProjects() {
   const app = useRealmApp();
   if (!app.currentUser) {
-    throw new Error("Cannot list projects if there is no logged in user.");
+    throw new Error('Cannot list projects if there is no logged in user.');
   }
-   // TODO: Retrieve the current user's projects and assign it to `projects`. 
-   const projects;
+  // TODO: Retrieve the current user's projects and assign it to `projects`.
+  const projects = app.currentUser.customData.memberOf;
   return projects;
 }
